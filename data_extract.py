557616b17df9d056608e0ec70e869bccc76ebea1
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-02-19 18:02:55 +0000

    Update data_extract.py

--- data_extract.py
+++ data_extract.py
@@ -7,26 +7,27 @@ soup = bs4.BeautifulSoup(req)
 
 film_sites = []
 film_names = []
+num_pages = int(1000/50)
+
 
 #get film names
 for link in soup.find_all('h3'):
 	print(link.text)
 
-for i in range(len(film_names)):
+for i in range(num_pages):
+	i += 1
+	url = 'http://www.imdb.com/search/title?groups=top_1000&page=%s&ref_=adv_prv' % num_pages
+	req = requests.get(url)
+	req.raise_for_status()
+	soup = bs4.BeautifulSoup(req.text)
+	#get film sites
+	for link in soup.find_all('h3'):
+	  try:
+	    x = 'imdb.com' + link.a['href']
+	    film_site.append(x)
+	  except TypeError:
+	    continue
+	#get film names
+	for link in soup.find_all('h3'):
+		print(link.text)
 
-
-#get film sites
-for link in soup.find_all('h3'):
-  try:
-    x = 'imdb.com' + link.a['href'])
-	  film_site.append(x)
-  except TypeError:
-    continue
-    
-#get the next page
-for link in soup.find_all('a', href=True):
-  try:
-	  if re.search('Next', link.text) is not None:
-		  print(link['href'])
-  except:
-    break 
