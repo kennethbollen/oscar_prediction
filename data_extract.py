1a134c2e91c9435869afc251719f4753d988b55f
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-02-20 15:21:49 +0000

    Update data_extract.py

--- data_extract.py
+++ data_extract.py
@@ -7,6 +7,7 @@ num_pages = int(round(4809/50)) + 1
 
 for i in range(num_pages):
 	i += 1
+	print('\ngathering data from page %s' %i)
 	url = 'http://www.imdb.com/search/title?groups=oscar_nominee&page=%s&ref_=adv_nxt' % num_pages
 	req = requests.get(url)
 	req.raise_for_status()
@@ -14,14 +15,24 @@ for i in range(num_pages):
 	#get film sites
 	for link in soup.find_all('h3'):
 	  try:
-	    x = 'imdb.com' + link.a['href']
+	    x = 'http://imdb.com' + link.a['href']
 	    x.strip(' ')
 	    film_sites.append(x)
 	  except TypeError:
 	    continue
-	#get film names
-	for link in soup.find_all('h3'):
-		x = link.text
-		x.strip(' ')
-		film_names.append(link.text)
 
+#clean the url list
+for i in range(len(film_names[i])):
+	if len(film_names[i]) > 40:
+		del(film_names[i])
+	else:
+		continue
+
+for site in film_sites:
+    req = requests.get(site)
+    req.raise_for_status()
+    soup = bs4.BeautifulSoup(req.text)
+    #runtime
+    for line in soup.find_all('time'):
+    	print(line.text[:3].strip())
+		
